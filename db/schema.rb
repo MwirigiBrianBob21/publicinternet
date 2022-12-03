40149d57169ee14f24d007b2dd094a608c700e49
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-12-03 20:41:00 +0300

    Add internet speeds table and related associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_171243) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_172529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "internet_speeds", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "place_id", null: false
+    t.float "download_speed", null: false
+    t.string "download_units", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_internet_speeds_on_place_id"
+  end
 
   create_table "places", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_171243) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "internet_speeds", "places"
 end
